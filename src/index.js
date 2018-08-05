10972e9420eebbc8dfdc0aa85e2961039b3795ce
alincode <alincode@gmail.com>
2018-08-06 00:15:14 +0800

    add css and event

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const html = require('nanohtml')
+var csjs = require('csjs-inject')
+
+const style = csjs`
+  .title {
+    font-size: 16px;
+    color: blue;
+  }
+`
 
 const el = html`
   <body>
     <h1>Hello world</h1>
-    <div>Click Me</div>
+    <div class="${style.title}">Hi everybody</div>
+    <button onclick=${onclick}>
+      Click Me
+    </button>
   </body>
 `
 
 document.body.appendChild(el)
+
+function onclick(e) {
+  console.dir(e);
+  console.log(`"${e.target.innerText}"" was clicked`)
+}
