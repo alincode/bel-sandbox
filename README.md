68a7b0d58ef4d7b82eac1763a278cc1451164657
alincode <alincode@gmail.com>
2018-08-06 22:03:22 +0800

    try more example

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 * [mattdesl/budo](https://github.com/mattdesl/budo)
 * [choojs/nanohtml](https://github.com/choojs/nanohtml)
+  * [wiki](https://github.com/choojs/nanohtml/wiki)
 * [ninabreznik/input-integer](https://github.com/ninabreznik/input-integer)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const html = require('nanohtml')
-var csjs = require('csjs-inject')
+const csjs = require('csjs-inject')
+const breadcrumb = require('breadcrumb-element')
+const morphdom = require('morphdom')
 
 const style = csjs`
   .title {
@@ -8,6 +10,7 @@ const style = csjs`
   }
 `
 
+// Compose an Element
 const el = html`
   <body>
     <h1>Hello world</h1>
@@ -20,7 +23,68 @@ const el = html`
 
 document.body.appendChild(el)
 
+// Attributes and Events
+
 function onclick(e) {
   console.dir(e);
   console.log(`"${e.target.innerText}"" was clicked`)
 }
+
+// Compose Multiple Elements
+
+var bears = ['grizzly', 'polar', 'brown']
+var element = html`<ul>
+  ${bears.map(function (bear) {
+    return html`<li>${bear}</li>`
+  })}
+</ul>`
+
+document.body.appendChild(element)
+
+// Updating an Element
+
+const element2 = html`
+  <div>Hi</div>
+`
+document.body.appendChild(element2)
+
+morphdom(element2, html`<div>changed!</div>`)
+
+// Data Down
+
+function render(msg) {
+  return html`<div>${msg}</div>`
+}
+var element3 = render('alincode')
+
+document.body.appendChild(element3)
+
+morphdom(element3, render('john'))
+
+// Scaling
+
+var bears = ['grizzly', 'polar', 'brown']
+var element4 = render2(bears)
+document.body.appendChild(element4)
+
+function render2(bears) {
+  return html`<ul>
+    ${bears.map(function (bear) {
+      return html`<li>${button(bear)}</li>`
+    })}
+  </ul>`
+}
+
+function button(label) {
+  return html`<button>${label}</button>`
+}
+
+
+
+// breadcrumb
+
+// const element = breadcrumb(['C', 'C++', 'JAVA'], function (picked) {
+//   console.log(picked)
+// })
+
+// document.body.appendChild(element)
